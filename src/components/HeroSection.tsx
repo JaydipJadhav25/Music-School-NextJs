5d1e3c13033dcbc09342d1d933846b3bf72ee02d
jaydipjadhav <jaydipdjadhav>
2024-08-10 16:46:05 +0530

    Herosection

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,27 +1,57 @@
+
 import React from "react";
 import { cn } from "../utils/cn";
 import { Spotlight } from "./ui/Spotlight";
+import Link from "next/link";
+import { Button } from "./ui/moving-border";
+
+
+{/* <Spotlight
+          className="-top-40 left-0 md:left-60 md:-top-20"
+          fill="red"
+        />
+        <Spotlight
+          className="-top-40 left-0 md:left-80 md:-top-40"
+          fill="white"
+        /> */}
 
 function HeroSection(){
-    return(
-        <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
-        <Spotlight
+    return (
+        <div
+        className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0"
+        >
+
+            <Spotlight
           className="-top-40 left-0 md:left-60 md:-top-20"
+          fill="red"
+        />
+        <Spotlight
+          className="-top-40 left-0 md:left-80 md:-top-40"
           fill="white"
         />
-        <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
-          <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
-            Spotlight <br /> is the new trend.
-          </h1>
-          <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
-            Spotlight effect is a great way to draw attention to a specific part
-            of the page. Here, we are drawing the attention towards the text
-            section of the page. I don&apos;t know why but I&apos;m running out of
-            copy.
-          </p>
+         <div className="p-4 relative z-10 w-full text-center" >
+            <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+            Master <br/> the art of music
+        </h1>
+        <p
+        className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto"
+            >Dive into our comprehensive music courses and transform your musical journey today. Whether you're a beginner or looking to refine your skills, join us to unlock your true potential.</p>
+
+        </div>    
+
+        <div className="mt-4">
+                <Link href={"/courses"}>
+                    <Button
+                    borderRadius="1.75rem"
+                    className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
+                    >
+                    Explore courses
+                    </Button>
+                </Link>
+            </div>
+            
         </div>
-      </div>
-    )
+      )
 }
 
 export default HeroSection;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,11 @@
 import type { Config } from "tailwindcss";
 
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+
 //  @type {import('tailwindcss').Config} 
 
 
@@ -35,6 +41,20 @@ const config: Config = {
     
     },
   },
-  plugins: [],
+  plugins: [
+    addVariablesForColors,
+  ],
 };
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
 export default config;
